d047220514ec4d2dd2db63035d5c5b9dd49eded8
Victor Robinson <victorandtina@hotmail.com>
2016-06-08 17:44:27 -0400

    Update packages

--- Foundations.WebApi/.Spritely.Recipes/CamelStrictConstructorContractResolver/CamelStrictConstructorContractResolver.cs
+++ Foundations.WebApi/.Spritely.Recipes/CamelStrictConstructorContractResolver/CamelStrictConstructorContractResolver.cs
@@ -29,8 +29,9 @@ namespace Spritely.Recipes
     [System.Diagnostics.DebuggerStepThrough]
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [System.CodeDom.Compiler.GeneratedCode("Spritely.Recipes", "See package version number")]
+#pragma warning disable 0436
 #endif
-    public class CamelStrictConstructorContractResolver
+    internal class CamelStrictConstructorContractResolver
         : CamelCasePropertyNamesContractResolver
     {
         private static readonly CamelStrictConstructorContractResolver ContractResolverInstance
@@ -92,4 +93,7 @@ namespace Spritely.Recipes
             return property;
         }
     }
+#if !RecipesProject
+#pragma warning restore 0436
+#endif
 }
--- Foundations.WebApi/.Spritely.Recipes/Functional/Functional.cs
+++ Foundations.WebApi/.Spritely.Recipes/Functional/Functional.cs
@@ -21,6 +21,7 @@ namespace Spritely.Recipes
     [System.Diagnostics.DebuggerStepThrough]
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [System.CodeDom.Compiler.GeneratedCode("Spritely.Recipes", "See package version number")]
+#pragma warning disable 0436
 #endif
     internal static partial class Functional
     {
@@ -64,4 +65,7 @@ namespace Spritely.Recipes
             source.Select(action.AsFunc()).ToList();
         }
     }
+#if !RecipesProject
+#pragma warning restore 0436
+#endif
 }
